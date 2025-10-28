87bab6e8eb84af35559b55ceec7ffdeb0566c12c
Innei <tukon479@gmail.com>
2025-10-28 19:39:58 +0800

    update
    Signed-off-by: Innei <tukon479@gmail.com>

---
+++ builder.config.ts
@@ -0,0 +1,17 @@
+import { defineBuilderConfig } from '@afilmory/builder'
+
+export default defineBuilderConfig(() => ({
+  repo: {
+    enable: true,
+    url: 'https://github.com/cupchino/afilmory-assets',
+  },
+  storage: {
+    provider: 's3',
+    bucket: 'gallery',
+    region: 'us-east-1',
+    prefix: '',
+    customDomain: 'https://r2.innei.ren',
+    endpoint:
+      'https://de7ecb0eaa0a328071255d557a6adb66.r2.cloudflarestorage.com',
+  },
+}))

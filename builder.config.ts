b92c40e8430797e79973447d679a70e83a862cd7
Innei <tukon479@gmail.com>
2025-10-28 19:51:15 +0800

    Add S3 credentials to builder configuration
    Signed-off-by: Innei <tukon479@gmail.com>

--- builder.config.ts
+++ builder.config.ts
@@ -10,6 +10,8 @@ export default defineBuilderConfig(() => ({
     bucket: 'gallery',
     region: 'us-east-1',
     prefix: '',
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     customDomain: 'https://r2.innei.ren',
     endpoint:
       'https://de7ecb0eaa0a328071255d557a6adb66.r2.cloudflarestorage.com',
